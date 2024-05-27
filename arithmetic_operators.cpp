e7fd4e940610d52cbf13c6b813143f97251a9143
Karan Singh Rana <104994569+Kahanhaikaran@users.noreply.github.com>
2024-05-27 13:36:38 +0530

    Create arithmetic_operators.cpp

---
+++ arithmetic_operators.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main() {
+  
+  // arithmetic operators = return the result of a specific arithmetic operation (+ - * /)
+  double students = 20;
+  //students = students +1;
+  //students+=1;
+
+  //students = students -1;
+  //students-=1;
+
+  //students = students *2;
+  //students*=2;
+
+  //students = students / 3;
+  //students = students/=3;
+  
+  int remainder = students % 2;
+  std::cout << remainder;
+  
+return 0
+}
