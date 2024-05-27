01efbbb08c625696ca3a0ccd08a15e0e5ef34acd
Karan Singh Rana <104994569+Kahanhaikaran@users.noreply.github.com>
2024-05-27 11:26:50 +0530

    variables.cpp

--- variables.cpp
+++ variables.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
 
+int main() {
+  int x; //declaration
+  x = 5; //assignment
+  int y = 6;
+
+  int sum = x+y;
+  
+  std::cout << x << '\n';
+  std::cout << y << '\n';
+  std::cout << sum;
+  return 0;
+}
