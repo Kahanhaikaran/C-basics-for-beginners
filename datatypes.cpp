bd07f2b41cb9bc7b0f6727a82cb8287778e6347c
Karan Singh Rana <104994569+Kahanhaikaran@users.noreply.github.com>
2024-05-27 11:45:50 +0530

    datatypes.cpp

---
+++ datatypes.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  //integer (whole number)
+  int age = 21;
+  int year = 2024;
+  int  days = 7;
+
+  //cout << age << '\n' << year << '\n' << days ;
+
+  //double (number including decimal)
+   double price = 10.99;
+   double petrol = 90.99;
+   double temperature = 25.1;
+
+   //cout << price << '\n' << petrol << '\n' << temperature;
+
+   //char (single character)
+
+   char grades = 'A';
+
+   cout << grades;
+
+   //boolean (true or false)
+
+   bool pass = false;
+
+   cout << pass;
+
+   //string (objects that represents a sequence of text)
+
+   string name = "Karan";
+   string day = "Monday";
+   string address = "165 JBP MP"
+   //cout << "Hello" << name;
+   return 0;
+}
