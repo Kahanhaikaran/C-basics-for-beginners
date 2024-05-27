255a5d1348760ed3840aeaa1a90fbd0aa5893d27
Karan Singh Rana <104994569+Kahanhaikaran@users.noreply.github.com>
2024-05-27 11:14:04 +0530

    hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,1 +1,7 @@
-#include <iostre
+#include <iostream>
+
+int main(){
+  std::cout << "I like mango!" << std::endl; //can add endl or '\n' to add a new line character 
+  std::cout << "It's really good!";
+return 0;
+}
