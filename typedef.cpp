cfb03de38e7cac1ea5ccb458620fce9788af42ae
Karan Singh Rana <104994569+Kahanhaikaran@users.noreply.github.com>
2024-05-27 13:25:56 +0530

    Update typedef.cpp

--- typedef.cpp
+++ typedef.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+#include <vector>
 
+//typedef std::vector<std::pair<std::string, int>> pairlist_t;
+//typedef std::string text_t;
+//typedef int number_t;
+
+using text_t = std::string;
+using number_t = int;
+int main(){
+
+  //pairlist_t pairlist;
+  text_t firstName = "Karan";
+  number_t age = 22;
+  
+  std::cout << firstName << '\n';
+  std::cout << age;
+
+  //typedef = reserved keyword used to create an additional name (alias) for another data type. New identifier for an existing type. Helps with readability and reduces typos. Use when there is a clear benefit. It can be replaced by 'using' (work better w/ templates)
+  
+  
+return 0;
+}
