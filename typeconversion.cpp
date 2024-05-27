353440890eacc83b8f7d0fa058a4349bc41ca1b7
Karan Singh Rana <104994569+Kahanhaikaran@users.noreply.github.com>
2024-05-27 17:17:06 +0530

    Create typeconversion.cpp

---
+++ typeconversion.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  //double x = (int)3.14;
+  //cout << x << '\n' ;
+  
+  //cout << (char)100 //give d as an output according to the ascii table
+  
+  int answers = 8;
+  int total = 10;
+  
+  int percentage = answers/(double)total * 100;
+  
+  cout << percentage << '%'; 
+return 0;
+  }
